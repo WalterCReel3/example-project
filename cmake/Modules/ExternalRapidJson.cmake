f9acf4ca7b3c712dffe958f5b177b696a5c37c9b
Walter Reel <waltercreel3rd@gmail.com>
2016-02-06 15:36:45 -0500

    RapidJson package tweaks

--- cmake/Modules/ExternalRapidJson.cmake
+++ cmake/Modules/ExternalRapidJson.cmake
@@ -2,6 +2,8 @@ ExternalProject_Add(
     rapidjson
     PREFIX third-party
     GIT_REPOSITORY https://github.com/miloyip/rapidjson.git
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}
+    CMAKE_ARGS
+	-DRAPIDJSON_BUILD_TESTS=OFF
+	-DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}
     BUILD_IN_SOURCE 1
 )
